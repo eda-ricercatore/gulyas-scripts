8514f7d85424997684ab9707d07f580b4e60359f
Zhiyang Ong <ongz@acm.org>
2016-10-03 09:23:49 -0500

    Added script for Instantaneous NBL.

--- time-shifting/time-shifting.m
+++ time-shifting/time-shifting.m
@@ -17,6 +17,16 @@ x2 = randi([0 1],1,array_length)
 %   Cross-correlation of x1 and x2
 [R, lag] = xcorr (x1, x2)
 
+avg_for 100 xcorr
+
+%   Time-shifted x1
+x3a = [9 1 2 3 4 5 6 7 8]
+horzcat = [0 0 0 x3a[1:1:5]]
+
+
+
+
+
 
 
 
