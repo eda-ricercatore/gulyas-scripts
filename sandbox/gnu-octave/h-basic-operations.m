a06d1e83d160d4ae4115bcea0c628a5f3aac9a15
Zhiyang Ong <ongz@acm.org>
2017-02-09 17:36:19 -0600

    Revise more basic operations: work in progress

--- sandbox/gnu-octave/h-basic-operations.m
+++ sandbox/gnu-octave/h-basic-operations.m
@@ -176,6 +176,26 @@ disp("	- PS2: Secondary prompt string.")
 
 disp("=	Fun with null matrices.")
 n1 = [1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16; 17 18 19 20; 21 22 23 24]
+if(isnull(n1))
+	disp("	Oops! 'n1' IS null.")
+else
+	disp("	= 'n1' is not null.")
+endif
+n2 = []
+if(isnull(n2))
+	disp("	= 'n2' is null.")
+else
+	disp("	Oops! 'n2' IS NOT null.")
+endif
+n3(n1) = []
+if(isnull(n3))
+	disp("	= 'n3' is null.")
+else
+	disp("	Oops! 'n3' IS NOT null.")
+endif
+n4 = []
+%n5(n1) = n3
+%n6(n1) = n3
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp("============================================================")
