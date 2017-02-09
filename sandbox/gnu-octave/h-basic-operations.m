53f8480da1add6b76a2d49804116a82ee1b3af83
Zhiyang Ong <ongz@acm.org>
2017-02-09 17:25:07 -0600

    Revise more basic operations.

--- sandbox/gnu-octave/h-basic-operations.m
+++ sandbox/gnu-octave/h-basic-operations.m
@@ -174,6 +174,9 @@ disp("	- OCTAVE_VERSION: Version of GNU Octave.")
 disp("	- PS1: Primary prompt string.")
 disp("	- PS2: Secondary prompt string.")
 
+disp("=	Fun with null matrices.")
+n1 = [1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16; 17 18 19 20; 21 22 23 24]
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp("============================================================")
 disp("	End:	Testing basic operations.")
