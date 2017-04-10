a892a8814f3ffc6197de1244a752d4a67feda212
Zhiyang Ong <ongz@acm.org>
2017-04-10 17:33:10 -0500

    Testing how to process BibTeX URL and DOI fields.

--- sandbox/python/process_bibtex_field.py
+++ sandbox/python/process_bibtex_field.py
@@ -29,3 +29,35 @@
 #	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"	Che cosa significa?
+
+###############################################################
+"""
+	List of strings representing common lines in my BibTeX database
+		that I have to process.
+"""
+# Backup URL field 1
+backup_url_field1_not_last_line = "Bdsk-Url-1 = {https://elifesciences.org/content/5/e16800},"
+backup_url_field1_not_last_line_doi = "Bdsk-Url-1 = {http://dx.doi.org/10.7554/eLife.13323},"
+backup_url_field1_last_line = "Bdsk-Url-1 = {https://www.babble.com/relationships/lessons-learned-about-racial-tolerance-from-the-only-white-kid-on-this-step-team/}}"
+backup_url_field1_last_line_doi = "Bdsk-Url-1 = {http://dx.doi.org/10.3386/w22347}}"
+# Backup URL field 2
+backup_url_field2_last_line_last_line_doi = "Bdsk-Url-2 = {http://dx.doi.org/10.7554/eLife.13323.001}}"
+backup_url_field2_last_line_last_line = "Bdsk-Url-2 = {http://doi.acm.org/10.1145/270955.270970}}"
+backup_url_field2_last_line = "Bdsk-Url-2 = {http://booksite.mkp.com/9780123838728/references.php}}"
+backup_url_field2_last_line_doi = "Bdsk-Url-2 = {http://dx.doi.org/10.1007/b117041}}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
