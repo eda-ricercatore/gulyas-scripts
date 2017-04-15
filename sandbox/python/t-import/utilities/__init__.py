ed3f29d26c34e113a3ae9444231ca1f63882956f
Zhiyang Ong <ongz@acm.org>
2017-04-14 20:25:25 -0500

    Add Python script to try importing modules from packages.

---
+++ sandbox/python/t-import/utilities/__init__.py
@@ -0,0 +1,9 @@
+###############################################################
+#	Import Custom Python Modules
+
+# Module to process input arguments to the script/program.
+#from utilities.queue_ip_arguments import queue_ip_args
+from . import queue_ip_arguments
+# Module to perform file I/O (input/output) operations.
+#from utilities.file_io import file_io_operations
+from . import file_io
