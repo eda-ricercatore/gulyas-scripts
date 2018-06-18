971210049dff4d6c1e10db83c3047aaa8c099f51
Zhiyang Ong <ongz@acm.org>
2018-06-18 11:33:04 -0500

    Update notes.

---
+++ std_cell_library/test_suite/generate_results_filename.py
@@ -0,0 +1,127 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+
+"""
+	This is written by Zhiyang Ong to generate a filename to store
+		experimental data/results from running experiments and/or
+		automated software regression testing and hardware regression
+		verification.
+
+
+
+	Synopsis: command name and [argument(s)]
+	./generate_results_filename.py [-h]
+
+	Parameters:
+	[input BibTeX file]:	A BibTeX database.
+
+	[-h]				:	If an optional "-h" flag is used as an
+							input argument, show the brief user manual
+							and exit (terminate the program).
+
+
+	Its procedure is described as follows:
+	Initialize an empty list of publishers.
+	Enumerate each line in the input BibTeX database.
+		If the currently enumerated line contains the 'Publisher'
+			BibTeX field,
+			Add its contents to set of publishers. 
+	Sort the set of publishers.
+
+	Notes/Assumptions:
+	Assume that the 'Publisher' standard BibTeX field is a single line
+		field.
+
+
+	Revision History:
+	April 17, 2017			Version 0.1, initial build.
+"""
+
+
+#	The MIT License (MIT)
+
+#	Copyright (c) <2014-2017> <Zhiyang Ong>
+
+#	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+#	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#	Email address: echo "cukj -wb- 23wU4X5M589 TROJANS cqkH wiuz2y 0f Mw Stanford" | awk '{ sub("23wU4X5M589","F.d_c_b. ") sub("Stanford","d0mA1n"); print $5, $2, $8; for (i=1; i<=1; i++) print "6\b"; print $9, $7, $6 }' | sed y/kqcbuHwM62z/gnotrzadqmC/ | tr 'q' ' ' | tr -d [:cntrl:] | tr -d 'ir' | tr y "\n"	Che cosa significa?
+
+#	==========================================================
+
+__author__ = 'Zhiyang Ong'
+__version__ = '0.2'
+__date__ = 'Apr 17, 2017'
+
+###############################################################
+
+"""
+	Import modules from The Python Standard Library.
+	sys			Get access to any command-line arguments.
+	os			Use any operating system dependent functionality.
+	os.path		For pathname manipulations.
+	
+	subprocess -> call
+				To make system calls.
+	time		To measure elapsed time.
+	warnings	Raise warnings.
+	re			Use regular expressions.
+	filecmp		For file comparison. 
+"""
+
+import sys
+import os
+import os.path
+from subprocess import call
+import time
+import warnings
+import re
+import filecmp
+
+###############################################################
+
+#	Import Custom Python Modules
+
+# Module to process input arguments to the script/program.
+#from utilities.queue_ip_arguments import queue_ip_args
+
+
+###############################################################
+class generate_filename:
+	@staticmethod
+	def create_filename():
+		"""
+			Generate filename to store experimental/simulation
+				results, from characterizing the standard cell
+				library that uses noise-based logic.
+		"""
+		print("")
+
+###############################################################
+# Main method for the program.
+
+#	If this is executed as a Python script,
+if __name__ == "__main__":
+	print("===================================================")
+	print("Generate filename to store experimental/simulation results.")
+	print("")
+	generate_filename.create_filename()
+	print("")
+	print("	= end =")
+
+
+#	Python database management
+#	Python: date, time, now, string
+#	Add references.
+#	Commit this.
+
+
+
+
+
+
+
+
+
