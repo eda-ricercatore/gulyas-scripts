6bd500da8c3513001c71e8c97570bec97d196800
Zhiyang Ong <ongz@acm.org>
2019-09-30 08:50:40 -0500

    Timing/performance measurements with Python 3.x, where x >= 3.

---
+++ sandbox/python/numbers/miscellaneous.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+
+
+# Python code to illustrate  
+# abs() built-in function 
+  
+# floating point number 
+float = -54.26
+print('Absolute value of float is:', abs(float)) 
+  
+# An integer 
+int = -94
+print('Absolute value of integer is:', abs(int)) 
+  
+# A complex number 
+complex = (3 - 4j) 
+print('Absolute value or Magnitude of complex is:', abs(complex)) 
+
