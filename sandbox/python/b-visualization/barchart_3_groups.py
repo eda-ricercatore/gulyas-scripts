5228f63f00f8fd370ecd2a5ea43238c97ac88ed6
Zhiyang Ong <ongz@acm.org>
2020-01-15 23:21:15 -0600

    Improve spacing between benchmarks.

--- sandbox/python/b-visualization/barchart_3_groups.py
+++ sandbox/python/b-visualization/barchart_3_groups.py
@@ -45,10 +45,10 @@ fig, ax = plt.subplots()
 	From https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.bar.html,
 		default width is 0.8.
 """
-rects1 = ax.bar(x - width, men_means, width=0.3, label='Men')
-rects2 = ax.bar(x, women_means, width=0.3, label='Women')
+rects1 = ax.bar(x - width, men_means, align='edge', width=0.3, label='Men')
+rects2 = ax.bar(x, women_means, align='edge', width=0.3, label='Women')
 # @modified by Zhiyang Ong, January 15, 2020.
-rects3 = ax.bar(x + width, other_means, width=0.3, label='Others')
+rects3 = ax.bar(x + width, other_means, align='edge', width=0.3, label='Others')
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Scores')
