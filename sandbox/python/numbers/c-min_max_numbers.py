73b993c85b2f9cea83197b3279ff84e4908c2ed1
Zhiyang Ong <ongz@acm.org>
2020-02-11 15:07:34 -0600

    Test minimum and maximum resolution of floating-point numbers and integers in Python.

---
+++ sandbox/python/numbers/c-min_max_numbers.py
@@ -0,0 +1,17 @@
+#!/Users/zhiyang/anaconda3/bin/python3
+###!/usr/local/bin/python3
+
+
+import sys
+
+
+
+
+print("DBL_MIN is",sys.float_info.min,".")
+print("DBL_MIN_10_EXP is",sys.float_info.min_10_exp,".")
+print("DBL_MIN_EXP is",sys.float_info.min_exp,".")
+print("DBL_MAX is",sys.float_info.max,".")
+print("negative DBL_MAX is",(-1*sys.float_info.max),".")
+print("DBL_MAX_10_EXP is",sys.float_info.max_10_exp,".")
+print("DBL_MAX_EXP is",sys.float_info.max_exp,".")
+print("sys.maxsize is",sys.maxsize,".")
