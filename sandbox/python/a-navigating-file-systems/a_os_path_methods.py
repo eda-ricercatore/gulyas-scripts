477988353e290e46f333309b09af8b740e3ffb58
Zhiyang Ong <ongz@acm.org>
2020-02-12 19:49:44 -0600

    Add information on working conditions for os.path.expanduser().

--- sandbox/python/a-navigating-file-systems/a_os_path_methods.py
+++ sandbox/python/a-navigating-file-systems/a_os_path_methods.py
@@ -38,7 +38,7 @@ print(f_extension1)
 
 
 """
-	References:
+	References and notes:
 	+ \cite[From section "File and Directory Access", subsection "os.path — Common pathname manipulations"]{DrakeJr2016b}
 		Available online from "The Python Standard Library: File and Directory Access: os.path — Common pathname manipulations" at: https://docs.python.org/3/library/os.path.html#os.path.expanduser;
 			February 12, 2020 was the last accessed date.
@@ -48,12 +48,21 @@ print(f_extension1)
 			* Hence, if the path does not start with "~" nor
 				"~user", or if the path cannot be expanded,
 				the 'path' remains unchanged.
-	+ 
-	Generic Operating System Services
+	+ \cite[From section "Generic Operating System Services", subsection "os — Miscellaneous operating system interfaces"]{DrakeJr2016b}
+		Available online from "The Python Standard Library: Generic Operating System Services: os — Miscellaneous operating system interfaces" at: https://docs.python.org/3/library/os.html#os.getcwd;
+		February 12, 2020 was the last accessed date.
 """
 valid_path = os.path.expanduser("./")
 print("valid_path is:",valid_path,"=")
+"""
+	See \cite[From section "File and Directory Access", subsection "os.path — Common pathname manipulations"]{DrakeJr2016b}
+		for reasons why the "os.path.expanduser(path)" command
+		does not work with the paths "./" nor ".".
+"""
 valid_path = os.path.expanduser(".")
 print("New valid_path is:",valid_path,"=")
+"""
+	See \cite[From section "Generic Operating System Services", subsection "os — Miscellaneous operating system interfaces"]{DrakeJr2016b}.
+"""
 current_wking_dir = os.getcwd()
 print("New current_wking_dir is:",current_wking_dir,"=")
