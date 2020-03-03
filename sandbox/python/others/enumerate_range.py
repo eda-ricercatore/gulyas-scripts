30abfbee5adbd22a1242aa0bc393e32dc5619855
Zhiyang Ong <ongz@acm.org>
2020-03-02 21:06:39 -0600

    Powers of 2 till max integer.

--- sandbox/python/others/enumerate_range.py
+++ sandbox/python/others/enumerate_range.py
@@ -17,6 +17,26 @@ powers_of_two = [ 2**power for power in range(3,10+1)]
 for i in powers_of_two:
 	print("Currently enumerated power of 2 is:",i,"=")
 
+print("- - - - - - - - - - - - - - - - - - -")
+
+for i in [ 2**power for power in range(3,10+1)]:
+	print("Currently enumerated power of 2 is:",i,"=")
+
+
+print("======================================")
+
+from itertools import count, takewhile
+for x in takewhile(lambda x: x <= 1024, (2**p for p in count(1))):
+    print("x via functional programming is:",x,"=")
+
+
+print("======================================")
+
+from math import log
+import sys
+for i in map(lambda v : pow(2,v), range(0,int(log(sys.maxint, 2)))):
+    print("Power of two till log(sys.maxint, 2) is:",i,"=")
+
 
 
 
