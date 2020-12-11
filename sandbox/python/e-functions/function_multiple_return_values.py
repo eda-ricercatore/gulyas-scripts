b0a82ce2e4550d4a81483d2f1dfeb8f07cdfdf65
Zhiyang Ong <ongz@acm.org>
2020-12-11 10:15:14 -0600

    Fix notes on functions returning multiple values.

--- sandbox/python/e-functions/function_multiple_return_values.py
+++ sandbox/python/e-functions/function_multiple_return_values.py
@@ -58,12 +58,39 @@ a, b, c = return_multiple_values()
 print("a is:",a,".")
 print("b is:",b,".")
 print("c is:",c,".")
+abc = return_multiple_values()
+print("abc is:",abc,".")
+print("	Is the type of 'abc' a tuple?:",type(abc) is tuple,"=")
+try:
+	ab, bc = return_multiple_values()
+	print("ab is:",ab,".")
+	print("bc is:",bc,".")
+except ValueError:
+	print("=	For functions that return more than 3 values,")
+	print("	their output can't be assigned to a subset of those values.")
+	print("=	In Python, multiple (& separate) return values automatically get grouped as a tuple.")
+	print("=	Tuples cannot be broken up separately with reassigning them to other variables")
+
 
 
 a1, b1, c1 = return_multiple_values_2()
 print("a1 is:",a1,".")
 print("b1 is:",b1,".")
 print("c1 is:",c1,".")
+abc1 = return_multiple_values_2()
+print("abc1 is:",abc1,".")
+try:
+	ab1, bc1 = return_multiple_values_2()
+	print("ab1 is:",ab1,".")
+	print("bc1 is:",bc1,".")
+except ValueError:
+	print(">	For functions that return more than 3 values,")
+	print("	their output can't be assigned to a subset of those values.")
+	print(">	In Python, multiple (& separate) return values automatically get grouped as a tuple.")
+	print(">	Tuples cannot be broken up separately with reassigning them to other variables")
+
+
+
 
 
 a_list = return_multiple_values_3()
@@ -76,9 +103,9 @@ print("my set is:",a_set,"=")
 
 a_dict = return_multiple_values_5()
 print("my dictionary is:",a_dict,"=")
-a = a_dict["xxx"]
-b = a_dict["yyy"]
-c = a_dict["zzz"]
-print("a is:",a,".")
-print("b is:",b,".")
-print("c is:",c,".")
+adic = a_dict["xxx"]
+bdic = a_dict["yyy"]
+cdic = a_dict["zzz"]
+print("adic is:",adic,".")
+print("bdic is:",bdic,".")
+print("cdic is:",cdic,".")
